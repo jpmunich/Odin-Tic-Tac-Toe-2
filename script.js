bf67b31cb64c6689285eed3d4f57e6ff15029755
jpmunich <parkercoyle8@gmail.com>
2023-07-17 11:38:50 -0400

    Added a turn system + Both players can alter data

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const tiles = document.querySelectorAll('.tile');
 
 
-const Player = (name, mark) => {
-    return { name, mark };
+const Player = (name, mark, isTurn) => {
+    return { name, mark, isTurn };
 }
 
 
@@ -13,19 +13,28 @@ const GameBoard = (() => {
 })()
 
 const GameController = (() => {
-    const createPlayer = (name, mark) => {return Player(name, mark)}
+    const createPlayer = (name, mark, isTurn) => {return Player(name, mark, isTurn)}
 
     tiles.forEach(element => {element.addEventListener('click', (e) => {
         updateBoard(e);
+        updateTurn();
     })})
 
     const updateBoard = (e) => {
-        GameBoard.board[e.target.getAttribute('data-index')] = playerOne.mark;
-        console.log(GameBoard.board)
+        if (playerOne.isTurn === true) GameBoard.board[e.target.getAttribute('data-index')] = playerOne.mark;
+        else if (playerTwo.isTurn === true) GameBoard.board[e.target.getAttribute('data-index')] = playerTwo.mark;
+    }
+
+    const updateTurn = () => {
+        if (playerOne.isTurn === true) {playerOne.isTurn = false; playerTwo.isTurn = true}
+        else if (playerTwo.isTurn === true) {playerOne.isTurn = true; playerTwo.isTurn = false}
     }
 
     return { createPlayer };
 })()
-const playerOne = GameController.createPlayer("Player X", "X");
+
+const playerOne = GameController.createPlayer('Player X', 'X', true);
+const playerTwo = GameController.createPlayer('Player O', 'O', false);
+
 
 
